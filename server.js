7bd0bb31b2ba13ef8cb988326698431ebae8235a
kconnell <connell.kate@ymail.com>
2019-02-21 23:49:32 -0500

    initial upload

---
+++ server.js
@@ -0,0 +1,29 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+var htmlRoutes =require("./app/routing/htmlRoutes");
+var apiRoutes = require("./app/routing/apiRoutes");
+
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+htmlRoutes(app);
+apiRoutes(app);
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
